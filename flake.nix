ea549cf031d4695213db9fccfc33d1a1f4e2ef41
hiina <hiina@hiina.space>
2026-09-11 15:39:13 -0600

    Pin native compiler environment including voice capture dependencies

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
     devShells.x86_64-linux.default = pkgs.mkShell {
       packages = with pkgs; [ cargo rustc rustfmt clippy pkg-config cmake ninja
         python3 scons clang lld patchelf binutils ffmpeg libpulseaudio ];
-      buildInputs = with pkgs; [ opus openssl ];
+      buildInputs = with pkgs; [ opus openssl alsa-lib ];
       LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [ pkgs.stdenv.cc.cc.lib pkgs.opus ];
     };
   };
